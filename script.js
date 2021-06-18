9d72885ff7c9f4df5080ce890d3a2515b94b13fd
eduardonk9999 <thiago.novak7@gmail.com>
2021-06-18 16:40:50 -0300

    Add, config

---
+++ script.js
@@ -0,0 +1,15 @@
+const teste = 0;
+const teste2 = 0;
+
+console.log(teste);
+console.log(teste2);
+
+
+const activeClass = "ativo"
+console.log(activeClass)
+
+function functionTeste() {
+
+}
+
+functionTeste()
